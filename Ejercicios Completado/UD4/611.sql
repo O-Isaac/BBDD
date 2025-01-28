d0e48469a7af3edc4ab0e1d2c2842107134c3591
O-Isaac <zaragozamendozaisaac@outlook.es>
2025-01-28 14:12:29 +0100

    I know, I know, this is not how I’m supposed to do it, but I can't think of something better.

---
+++ Ejercicios Completado/UD4/611.sql
@@ -0,0 +1,15 @@
+-- Muestra un listado de todas las habilidades, y si tienen, el código del empleado y el nivel de su habilidad.
+
+select * from habilidad s 
+    left join habemp h on h.CodHab = s.CodHab;
+
+-- Muestra un listado de las habilidades que no tiene ningún empleado.
+
+select * from habilidad s 
+    left join habemp h on h.CodHab = s.CodHab
+    where h.CodEmp is null;
+
+-- Comprueba si los empleados que no tienen hijos (empleado.NumHi) coinciden con los empleados no tienen hijos relacionados (tabla hijo).
+
+select e.* from empleado e
+    where e.NumHi = 0 and e.CodEmp not in (select CodEmp from hijo);
